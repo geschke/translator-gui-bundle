e976face266e264337967b05d42497a56f23f7fc
Ralf Geschke <ralf@kuerbis.org>
2014-11-18 23:30:59 +0100

    rg: read directory contents as first test...

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -14,26 +14,64 @@ class DefaultController extends Controller
 
         $request->setLocale('de');
 
-        $translator =  $this->get('translator');
+        $translator = $this->get('translator');
         $welcome = $translator->trans("Welcome to Translator GUI Bundle!");
 
         $name = 'foo';
         return $this->render('GeschkeAdminTranslatorGUIBundle:Default:index.html.twig',
-            array('mainnav' => '',
+            array(
+                'mainnav' => '',
                 'name' => $name,
-            'welcome' => $welcome));
+                'welcome' => $welcome
+            ));
+    }
+
+    public function getLanguages($path)
+    {
+        try {
+            $files = new \DirectoryIterator($path . 'Resources/translations/');
+            echo $path . "\n";
+            foreach ($files as $fileinfo) {
+                if (!$fileinfo->isDot()) {
+                    var_dump($fileinfo->getFilename());
+                }
+            }
+
+        }
+        catch (\UnexpectedValueException $e) {
+            echo "no entry for $path\n";
+        }
+
+        return $foo = true;
     }
 
     public function listBundlesAction()
     {
         $bundles = $this->container->getParameter('kernel.bundles');
 
+        $kernel = $this->container->get('kernel');
 
+
+        foreach ($bundles as $bundle => $bundleFullName) {
+            $path = $kernel->locateResource('@' . $bundle);
+            $this->getLanguages($path);
+            $bundleList[] = array(
+                'name' => $bundle,
+                'fullName' => $bundleFullName,
+                'path' => $path
+            );
+
+
+        }
+        asort($bundleList);
+        die;
         $name = 'foo';
         return $this->render('GeschkeAdminTranslatorGUIBundle:Bundles:list.html.twig',
-            array('mainnav' => '',
-                'bundles' => $bundles,
-                'name' => $name));
+            array(
+                'mainnav' => '',
+                'bundles' => $bundleList,
+                'name' => $name
+            ));
     }
 
 }
